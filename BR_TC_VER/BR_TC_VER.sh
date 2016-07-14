780218c5d7c8429bf284c580ab23649ce39219f7
Vlad Zakharov <vzakhar@synopsys.com>
2016-07-14 17:13:01 +0300

    BR_TC_VER: Old version auto-detection and default buildroot path
    Old version now is detected automatically using regular expressions.
    
    Also defauilt buildroot path was added. Script now checks does the
    buildroot directory exists. If not it exits with error.
    
    According to these changes -o [old version] and -n [new version]
    options are not mandatory now.

--- BR_TC_VER/BR_TC_VER.sh
+++ BR_TC_VER/BR_TC_VER.sh
@@ -4,18 +4,36 @@
 
 # Help sctring
 
-help_str="Usage:\n\n
+help_str=" Usage:\t
 ./BR_TC_VER -o [old_version] -n [new_version] -p [path_to_buildroot]\n\n
 NOTE: -o and -p are requred options, missing them will lead to failures\n\n
 \t[old_version]\t\t\tshould be something like \"arc-YYYY.MM\"\n
 \t[new_version]\t\t\tshould be something like \"arc-YYYY.MM\"\n
-\t[path_to_buildroot]\t\tis path to \"buildroot\" root directory\n"
+\t[path_to_buildroot]\t\tis path to \"buildroot\" root directory\n\n
+One more note should be added - after update you need to check everyhing\n
+manually, especially custom patches as they may not be required.\n"
 
 # Script variables with default values
-old=""                           # Required option
+old=""
 version="arc-2016.03"                      
-buildroot_path=""                # Required option
+buildroot_path="$HOME/git/buildroot"
 sha_sum=none
+
+# Files where the script tries to find old arc toolchain version
+search_files="binutils/Config.in.host binutils/binutils.mk binutils/binutils.hash gcc/Config.in.host gcc/gcc.hash"
+
+function detect_version {
+    for file in $search_files
+    do
+	old=`egrep -o "arc-[0-9]{4}\.[0-9]{2}(-eng[0-9]{3})?" $buildroot_path/package/$file`
+	if [ $? -eq 0  ];
+	then
+	    echo "Old version detected: $old"
+	    return 0;
+	fi
+    done
+    return 1;
+}
 
 # Get the command line parametres
 while getopts ":n:p:o:h" OPTION; do
@@ -31,24 +49,35 @@ while getopts ":n:p:o:h" OPTION; do
 	    ;;
 	h)
 	    echo -e $help_str
-	    exit 1
+	    exit 0
 	    ;;
 	\?)
 	    echo "No such argument"
+	    exit 1
 	    ;;
     esac
 done
 
-if [ -z $old ];
+# Verify if buildroot directory exists
+if [ ! -d $buildroot_path ]
 then
-    echo "You must specify old version of toolchain to run the script"
+    echo 1>&2 "Buidroot directory doesn't exist. Plese specify correct path to buildroot. For more info run \"./BR_TC_VER -h\" ."
     exit 1
 fi
 
-if [ -z $buildroot_path ];
+# Detect old version to be updated
+detect_version
+if [ $? -ne 0 ]
 then
-    echo "You must specify paht to buildroot to run the script"
+    echo 1>&2 "Version autodetection filed. Please specify old version you want to update."
     exit 1
+fi
+
+# No sense to update if old version matches with the new version. 
+if [ "$old" == "$version" ]
+then
+    echo "Old version coinsides with new version. No sense to update"
+    exit 0
 fi
 
 
