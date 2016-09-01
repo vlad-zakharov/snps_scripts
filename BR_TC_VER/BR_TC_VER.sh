b6e9f6563ae37f097c132d38ff96c971d85adcf6
Vlad Zakharov <vzakhar@synopsys.com>
2016-09-01 10:43:41 +0300

    BR_TC_VER: revert changes if script fails
    Some work may have been done if the script fails for some reason.
    So some files could be updated when some could stay in obsolete
    state. This is not a correct behaviour so we had to revert all
    changes if the script fails.

--- BR_TC_VER/BR_TC_VER.sh
+++ BR_TC_VER/BR_TC_VER.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # NOTE: The script is suited to update only arc toolchain
+# NOTE: If scipt fails, it reverts ALL your local changes. So use it only after you have commited all your changes
 
-# Help sctring
+# Help string
 
-help_str=" Usage:\t
+help_str="\n Use only after you have commited ALL your local changes!\n\n
+ Usage:\t
 ./BR_TC_VER -o [old_version] -n [new_version] -p [path_to_buildroot]\n\n
-NOTE: -o and -p are requred options, missing them will lead to failures\n\n
 \t[old_version]\t\t\tshould be something like \"arc-YYYY.MM\"\n
 \t[new_version]\t\t\tshould be something like \"arc-YYYY.MM\"\n
 \t[path_to_buildroot]\t\tis path to \"buildroot\" root directory\n\n
@@ -33,6 +34,14 @@ function detect_version {
 	fi
     done
     return 1;
+}
+
+# This function reverts all local changes
+# It is called if script is exited due to some error, exception or signal
+function revert_changes {
+    cd $buildroot_path
+    git reset HEAD . && git checkout -- . && git clean -fd
+    return $(echo $?)
 }
 
 # Get the command line parametres
@@ -69,7 +78,7 @@ fi
 detect_version
 if [ $? -ne 0 ]
 then
-    echo 1>&2 "Version autodetection filed. Please specify old version you want to update."
+    echo 1>&2 "Version autodetection failed. Please specify old version you want to update."
     exit 1
 fi
 
@@ -80,45 +89,75 @@ then
     exit 0
 fi
 
-
-
-#### Update binutils ####
-cd $buildroot_path/package/binutils
-sed "s/$old/$version/g" Config.in.host > Config.in.host.temp
-mv Config.in.host.temp Config.in.host
-
-sed "s/$old/$version/g" binutils.mk > binutils.mk.temp
-mv binutils.mk.temp binutils.mk
-
-git mv $old $version
-
 # Update sha hash for new binutils
+cd $buildroot_path/package/binutils && \
 if test -e "../../dl/binutils-$version.tar.gz"
 then
     sha_sum=$(sha512sum "../../dl/binutils-$version.tar.gz" | awk '{print $1;}')
 else
-    wget -P ../../dl "https://github.com/foss-for-synopsys-dwc-arc-processors/binutils-gdb/archive/$version/binutils-$version.tar.gz"
+    wget -P ../../dl "https://github.com/foss-for-synopsys-dwc-arc-processors/binutils-gdb/archive/$version/binutils-$version.tar.gz" && \
     sha_sum=$(sha512sum "../../dl/binutils-$version.tar.gz" | awk '{print $1;}')
 fi
-sed "s/sha512.*binutils-${old}.tar.gz/sha512\ \ ${sha_sum}\ \ binutils-${version}.tar.gz/g" binutils.hash > binutils.hash.temp
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
+
+sed "s/sha512.*binutils-${old}.tar.gz/sha512\ \ ${sha_sum}\ \ binutils-${version}.tar.gz/g" binutils.hash > binutils.hash.temp && \
 mv binutils.hash.temp binutils.hash
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
 
-
-
-#### Update gcc ####
-cd $buildroot_path/package/gcc
-sed "s/$old/$version/g" Config.in.host > Config.in.host.temp
-mv Config.in.host.temp Config.in.host
-
-git mv $old $version
 
 # Update sha hash for new gcc
+cd $buildroot_path/package/gcc && \
 if test -e "../../dl/gcc-$version.tar.gz"
 then
     sha_sum=$(sha512sum "../../dl/gcc-$version.tar.gz" | awk '{print $1;}')
 else
-    wget -P ../../dl "https://github.com/foss-for-synopsys-dwc-arc-processors/gcc/archive/$version/gcc-$version.tar.gz"
+    wget -P ../../dl "https://github.com/foss-for-synopsys-dwc-arc-processors/gcc/archive/$version/gcc-$version.tar.gz" && \
     sha_sum=$(sha512sum "../../dl/gcc-$version.tar.gz" | awk '{print $1;}')
 fi
-sed "s/sha512.*gcc-${old}.tar.gz/sha512\ \ ${sha_sum}\ \ gcc-${version}.tar.gz/g" gcc.hash > gcc.hash.temp
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
+
+
+sed "s/sha512.*gcc-${old}.tar.gz/sha512\ \ ${sha_sum}\ \ gcc-${version}.tar.gz/g" gcc.hash > gcc.hash.temp && \
 mv gcc.hash.temp gcc.hash
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
+
+# Update binutils
+cd $buildroot_path/package/binutils && \
+sed "s/$old/$version/g" Config.in.host > Config.in.host.temp && \
+mv Config.in.host.temp Config.in.host && \
+sed "s/$old/$version/g" binutils.mk > binutils.mk.temp && \
+mv binutils.mk.temp binutils.mk && \
+git mv $old $version
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
+
+# Update gcc
+cd $buildroot_path/package/gcc && \
+sed "s/$old/$version/g" Config.in.host > Config.in.host.temp && \
+mv Config.in.host.temp Config.in.host && \
+git mv $old $version
+if [ $? -ne 0 ]
+then
+    revert_changes
+    exit 1
+fi
+
